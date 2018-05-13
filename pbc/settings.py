dd2b8d2d45c1a2de4c1b925c8d3866d9c5180f7d
wofstech <gabrielabuka3@gmail.com>
2018-05-12 19:58:24 -0600

    'added host'

--- pbc/settings.py
+++ pbc/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+ytdjydtj9tax!#a3cup@1$8
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immense-cliffs-27305.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
